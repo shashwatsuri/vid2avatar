11896587e30503318cfd328ecff07d472088ba66
Shashwat Suri <shashwatsuri98@gmail.com>
2024-01-08 15:22:17 -0800

    added "gpus" option

--- code/test.py
+++ code/test.py
@@ -19,7 +19,7 @@ def main(opt):
     logger = WandbLogger(project=opt.project_name, name=f"{opt.exp}/{opt.run}")
 
     trainer = pl.Trainer(
-        # gpus=1,
+        gpus=1,
         accelerator="gpu",
         callbacks=[checkpoint_callback],
         max_epochs=4000,
